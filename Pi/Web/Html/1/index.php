6fcaeea3c858e9263da2981727d6d59d8b9bd2d3
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2022-06-23 11:02:45 +0200

    Update index.php

--- Pi/Web/Html/1/index.php
+++ Pi/Web/Html/1/index.php
@@ -1,5 +1,5 @@
 <?php
-$version_html ="0.0.10";
+$version_html ="0.0.11";
 #########################################################
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
@@ -205,7 +205,19 @@ $all_connect = shell_exec('arp');
 $all_connect_n = shell_exec('arp -n');
 ?>	
 <p>All connect N:</p> <?php echo "<pre>$all_connect_n</pre>";?><hr>
-
+<?php
+$MemTotal = shell_exec('awk '/MemTotal/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo');
+?>	
+<p>MemTotal :</p> <?php echo "<pre>$MemTotal Mo</pre>";?><hr>
+<?php
+$MemFree = shell_exec('awk '/MemFree/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo');
+?>	
+<p>MemFree :</p> <?php echo "<pre>$MemFree Mo</pre>";?><hr>
+<?php
+$MemAvailable = shell_exec('awk '/MemAvailable/ {printf( "%d\n", $2 / 1024 )}' /proc/meminfo');
+?>	
+<p>MemAvailable :</p> <?php echo "<pre>$MemAvailable Mo</pre>";?><hr>
+	
 </div>
 <footer class="mt-5">
 <div class="py-5 bg-dark ">
